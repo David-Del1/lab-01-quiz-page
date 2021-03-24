995adf08c254c27fbb1e957211a47eb5e6067445
David Delgadillo <daviddel.731@gmail.com>
2021-03-24 08:02:35 -0700

    HTML, CSS, and JS complete.

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 // import functions and grab DOM elements
 
+import {countsAsAYes} from './utils.js';
 // initialize state
 
 // set event listeners to update state and DOM
+
+const startBtn = document.getElementById('startBtn');
+const outcomeDiv = document.getElementById('outcomeDiv');
+
+startBtn.addEventListener('click', () => {
+    alert('Welcome to the Octopus quiz!');
+
+
+
+    const readyToTakeQuiz = confirm('Would you like to start the quiz now?');
+
+    if (!readyToTakeQuiz) {
+        alert('Come back when you are ready to take the octo-challenge.');
+        return;
+    }
+
+    const name = prompt('What is your name?')
+
+    let score = 0;
+
+    const firstAnswer = prompt('Is an octopus a considered a Crustacean');
+    const secondAnswer = prompt('Is an octopus considered one of the most intelligent invertebrates on earth?');
+    const thirdAnswer = prompt('Are all octopuses deadly to humans?');
+
+    if(!countsAsAYes(firstAnswer)) {
+        score++;
+    }
+    if(countsAsAYes(secondAnswer)) {
+        score++;
+    }
+    if(!countsAsAYes(thirdAnswer)) {
+        score++;
+    }
+
+    const outcomeString = `Okay ${name}, you got ${score} right out of 3.`;
+
+    outcomeDiv.textContent = outcomeString;
+
+
+});
+
+
+// USE the recipe
